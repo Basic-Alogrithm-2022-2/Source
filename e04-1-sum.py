b344205aedb7b9c9f307f3a7959d7156d0943c68
김종현|Jonghyun Kim <jhkim@dit.ac.kr>
2022-10-19 22:33:39 +0900

    update

---
+++ e04-1-sum.py
@@ -0,0 +1,18 @@
+# 연속한 숫자의 합을 구하는 알고리즘
+# 입력: n
+# 출력: 1부터 n까지 연속한 숫자를 더한 합
+
+def sum(n):
+    if n == 0:
+        return 0
+    return sum(n - 1) + n
+
+def sumfor(n):
+    i = 1
+    sum = 0
+    for i in range(1,n+1):
+        sum = sum + i
+    return sum
+
+print(sum(10))   # 1부터 10까지의 합(입력:10, 출력:55)
+print(sumfor(10))  # 1부터 100까지의 합(입력:100, 출력:5050)
